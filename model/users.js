f8fd8053487bb4d43c0136670ffb6b7f420d16e9
James-Hardy <jamesjhardy95@gmail.com>
2020-04-16 18:57:01 +0100

    merged master into branch

--- model/users.js
+++ model/users.js
@@ -1,39 +1,40 @@
-const db = require("../database/connection");
-
+const db = require('../database/connection')
 
 function createUser(user) {
-  return db.query("INSERT INTO users(username, email, password) VALUES ($1,$2,$3)", [user.username, user.email, user.password])
-  .then(() => {
-    return getUser(user.email)
-  })
-  .catch(error => error)
-
+  return db
+    .query('INSERT INTO users(username, email, password) VALUES ($1,$2,$3)', [
+      user.username,
+      user.email,
+      user.password,
+    ])
+    .then(() => {
+      return getUser(user.email)
+    })
+    .catch(error => error)
 }
 
-function editUser(user) {
+function editUser(user) {}
 
-}
-
-function deleteUser(user) {
-
-}
+function deleteUser(user) {}
 
 function getUser(email) {
-  return db.query("SELECT * FROM users WHERE email=($1)", [email])
-        .then(user => user.rows[0])
-        .catch(error => error)
+  return db
+    .query('SELECT * FROM users WHERE email=($1)', [email])
+    .then(user => user.rows[0])
+    .catch(error => error)
 }
 
 function getUserById(id) {
-  return db.query("SELECT * FROM users WHERE id=($1)", [id])
-        .then(user => user.rows[0])
-        .catch(error => console.error(error))
+  return db
+    .query('SELECT * FROM users WHERE id=($1)', [id])
+    .then(user => user.rows[0])
+    .catch(error => console.error(error))
 }
 
 module.exports = {
-    createUser,
-    editUser,
-    deleteUser,
-    getUser,
-    getUserById
+  createUser,
+  editUser,
+  deleteUser,
+  getUser,
+  getUserById,
 }
--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require('express')
 
 // Handler modulles
-const runs = require("./handlers/runs")
-const users = require("./handlers/users")
-const teams = require("./handlers/teams")
+const runs = require('./handlers/runs')
+const users = require('./handlers/users')
+const teams = require('./handlers/teams')
 
 // Middleware
-const handleError = require('./middleware/error');
+const handleError = require('./middleware/error')
 // const getUser = require("./middleware/getUser");
-const authorize = require("./middleware/authorize");
-const logger = require("./middleware/logger");
+const authorize = require('./middleware/authorize')
+const logger = require('./middleware/logger')
 
 const PORT = process.env.PORT || 3000
 
 const server = express()
-server.use(express.json());
+server.use(express.json())
 // server.use(getUser);
-server.use(logger);
+server.use(logger)
 
 // Request handling
 // GET
-server.get("/users/runs/", runs.getAllMyRuns)
-server.get("/users/runs/:date1/:date2", runs.getRunsInDates)
-server.get("/users/runs/:runId", runs.getSpecificRun)
+server.get('/users/runs/', runs.getAllMyRuns)
+server.get('/users/runs/:date1/:date2', runs.getRunsInDates)
+server.get('/users/runs/:runId', runs.getSpecificRun)
 // server.get("/teams", teams.getAllTeams)
 // server.get("/teams/:teamName", teams.getTeamMembersFromTeamName)
 
 // POST
-server.post("/login", users.logIn)
-server.post("/create/user", users.createUser)
-server.post("/create/team/:team/:distance", authorize, teams.createTeam)
+server.post('/login', users.logIn)
+server.post('/create/user', users.createUser)
+server.post('/create/team/:team/:distance', authorize, teams.createTeam)
 // server.post("/create/user/:run", authorize, runs.createRun)
 
 // PUT
@@ -37,12 +37,10 @@ server.post("/create/team/:team/:distance", authorize, teams.createTeam)
 // server.put('/update/users/:id', authorize, users.editUser)
 server.put('/update/team/:team', authorize, teams.editTeam)
 
-
 // DELETE
 // server.delete('/delete/runs/:id', authorize, runs.deleteRun)
 // server.delete('/delete/users/:id', authorize, users.deleteUser)
 // server.delete('/delete/teams/:id', authorize, teams.deleteTeam)
-
 
 server.use(handleError)
 
