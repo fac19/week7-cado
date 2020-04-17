4c3acd6605b08767ecf04ffb1a4492e46b68532f
James-Hardy <jamesjhardy95@gmail.com>
2020-04-17 09:28:49 +0100

    add get userid from payload for get all runs and runs in date

--- handlers/runs.js
+++ handlers/runs.js
@@ -6,25 +6,52 @@ dotenv.config()
 const SECRET = process.env.JWT_SECRET
 
 function getAllMyRuns(req, res, next) {
-  const userId = 3
-  modelRuns
-    .getAllMyRuns(userId)
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const error = new Error("Authorization header required");
+    error.status = 400;
+    next(error);
+  }
+  const token = authHeader.replace("Bearer ", "");
+  try {
+    const data = jwt.verify(token, SECRET);
+    modelRuns
+    .getAllMyRuns(data.user)
     .then(runs => {
       res.send(runs)
     })
     .catch(next)
+  } catch (_error) {
+    const error = new Error("Invalid token");
+    error.status = 401;
+    next(error);
+  }
 }
 
 function getRunsInDates(req, res, next) {
-  const userId = 1
+  const authHeader = req.headers.authorization;
   const date1 = req.params.date1
   const date2 = req.params.date2
-  modelRuns
-    .getRunsInDates(userId, date1, date2)
+
+  if (!authHeader) {
+    const error = new Error("Authorization header required");
+    error.status = 400;
+    next(error);
+  }
+  const token = authHeader.replace("Bearer ", "");
+  try {
+    const data = jwt.verify(token, SECRET);
+     modelRuns
+    .getRunsInDates(data.user, date1, date2)
     .then(runs => {
       res.send(runs)
     })
     .catch(next)
+  } catch (_error) {
+    const error = new Error("Invalid token");
+    error.status = 401;
+    next(error);
+  }
 }
 
 function getSpecificRun(req, res, next) {
